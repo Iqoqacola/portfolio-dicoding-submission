2b2db1f503f6eb9fc96055e3229e1b9eb3e7fd88
Muhammad Iqbal <147232852+Iqoqacola@users.noreply.github.com>
2024-02-04 21:52:17 +0800

    Upload File
    Menambahakn file website portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+function hoverEffect(element) {
+    // Saat dihover, perbesar elemen
+    element.style.transform = "scale(1.1)";
+  }
+
+  function unhoverEffect(element) {
+    // Saat keluar dari hover, kembalikan ukuran elemen ke semula
+    element.style.transform = "scale(1)";
+  }
